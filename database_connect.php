98a3dda367beeb2bf7ae33c6821d6797260796e1
ElmarGajiev <111605032+ElmarGajiev@users.noreply.github.com>
2022-08-22 03:41:20 +0400

    Add files via upload

---
+++ database_connect.php
@@ -0,0 +1,7 @@
+<?php
+	$servername="localhost";
+	$username="id19440460_product";
+	$password="Elmar-12345678";
+	$dbname="id19440460_products";
+	$connect=new mysqli($servername, $username, $password, $dbname);
+?>
