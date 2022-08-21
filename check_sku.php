a2c978edc1156a7cc7bec3b761d0137f1d292bbe
ElmarGajiev <111605032+ElmarGajiev@users.noreply.github.com>
2022-08-22 03:42:20 +0400

    Update check_sku.php

--- check_sku.php
+++ check_sku.php
@@ -1,9 +1,5 @@
 <?php
-	$servername="localhost";
-	$username="id19440460_product";
-	$password="Elmar-12345678";
-	$dbname="id19440460_products";
-	$connect=new mysqli($servername, $username, $password, $dbname);
+	require_once("database_connect.php");
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
