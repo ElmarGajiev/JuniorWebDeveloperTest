5eb29344745c65449768c315d17cffae16dc4e4b
ElmarGajiev <111605032+ElmarGajiev@users.noreply.github.com>
2022-09-06 18:46:39 +0400

    Add files via upload

---
+++ database/check_sku.php
@@ -0,0 +1,18 @@
+<?php
+	require_once($_SERVER["DOCUMENT_ROOT"] ."/public/database/connect.php");
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+		$sku=$_POST["sku"];
+		$sql = "Select sku from product_list where sku='".$sku."'";;
+		$result=$connect->query($sql);
+		$row=$result->fetch_assoc();
+
+		if($row==Null){
+			echo "valid";
+		}
+		else {
+			echo "invalid";
+		}
+	}
+?>
---
+++ database/delete_product.php
@@ -0,0 +1,21 @@
+<?php
+	require_once($_SERVER["DOCUMENT_ROOT"] ."/public/database/connect.php");
+	
+
+	if ($_SERVER["REQUEST_METHOD"]=="POST"){
+
+		$sku = $_POST["check"];
+		foreach ($sku as $check){ 
+			$sql1="DELETE FROM product_list WHERE sku='".$check."'";
+			$sql2="DELETE FROM book WHERE sku='".$check."'";
+			$sql3="DELETE FROM dvd WHERE sku='".$check."'";
+			$sql4="DELETE FROM furniture WHERE sku='".$check."'";
+			$connect->query($sql1);
+			$connect->query($sql2);
+			$connect->query($sql3);
+			$connect->query($sql4);
+		}
+
+		header("Location: /public/index.php");
+	}
+?>
