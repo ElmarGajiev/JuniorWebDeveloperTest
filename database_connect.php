b3201e7fee4bf40fab87f06e24181d7161b1279f
ElmarGajiev <111605032+ElmarGajiev@users.noreply.github.com>
2022-09-06 18:37:39 +0400

    Delete database_connect.php

--- database_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$servername="localhost";
-	$username="id19440460_product";
-	$password="Elmar-12345678";
-	$dbname="id19440460_products";
-	$connect=new mysqli($servername, $username, $password, $dbname);
-?>
