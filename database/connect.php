c20e5bf42933081828b1c388fec63c0ba092b251
ElmarGajiev <111605032+ElmarGajiev@users.noreply.github.com>
2022-09-17 14:43:59 +0400

    Delete database directory

--- database/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "products";
-    $connect = new mysqli($servername, $username, $password, $dbname);
