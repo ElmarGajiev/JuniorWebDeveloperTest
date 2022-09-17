ba023627800c8b143aa635a821f7ff35cb183d03
ElmarGajiev <111605032+ElmarGajiev@users.noreply.github.com>
2022-09-17 22:31:08 +0400

    Add files via upload

---
+++ database/classes/connect/connect_class.php
@@ -0,0 +1,14 @@
+<?php
+Namespace connect;
+class Connect {
+    public $connect;
+    public function __construct($connect){
+        $this->connect = $connect;
+    }
+    public function connect(){
+        $params = func_get_args();
+        foreach ($params as $sql){
+            $this->connect->query($sql);
+        }
+    }
+}
